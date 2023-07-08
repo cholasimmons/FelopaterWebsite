78165f721d2b7e03bdf21238b7ed99764dab0d92
JustChola <simmonsfrank@gmail.com>
2023-07-09 00:29:39 +0200

    Projects carousel on homepage

--- dist/js/card-carousel.js
+++ dist/js/card-carousel.js
@@ -1,17 +1,21 @@
 const config = {
     type: 'carousel',
-    startAt: 0,
+    startAt: 1,
     perView: 4,
-    gap: 32,
+    gap: 4,
+    focusAt: 'center',
+    autoplay: '4800',
+    hoverpause: true,
+    keyboard: true,
     breakpoints: {
       1280: {
-        perView: 3,
+        perView: 4,
       },
       1024: {
-        perView: 2,
+        perView: 3,
       },
       768: {
-        perView: 1,
+        perView: 2,
       }
     }
   }
